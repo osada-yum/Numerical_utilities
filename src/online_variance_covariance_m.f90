949d398ccf58e99e22d7c7fcee1246111e527799
osada-yum <osada.yum@gmail.com>
2024-01-23 16:21:36 +0900

    Bug fixed. rename.
    	modified:   src/online_variance_covariance_m.f90

--- src/online_variance_covariance_m.f90
+++ src/online_variance_covariance_m.f90
@@ -24,7 +24,7 @@ module online_variance_covariance_m
      module procedure :: generate_online_variance_covariance
   end interface online_variance_covariance
 contains
-  pure type(online_covariance) function generate_online_variance_covariance(n, arr1, arr2) result(res)
+  pure type(online_variance_covariance) function generate_online_variance_covariance(n, arr1, arr2) result(res)
     integer(int32), intent(in) :: n
     real(real64), intent(in) :: arr1(n), arr2(n)
     integer(int32) :: i
@@ -33,7 +33,7 @@ contains
     end do
   end function generate_online_variance_covariance
   pure subroutine add_pair_ovc(this, v1, v2)
-    class(online_covariance), intent(inout) :: this
+    class(online_variance_covariance), intent(inout) :: this
     real(real64), intent(in) :: v1, v2
     this%cnts_ = this%cnts_ + 1
     associate(d1 => v1 - this%mean1_, d2 => v2 - this%mean2_)
@@ -47,35 +47,35 @@ contains
     end associate
   end subroutine add_pair_ovc
   pure real(real64) function mean1_ovc(this) result(res)
-    class(online_covariance), intent(in) :: this
+    class(online_variance_covariance), intent(in) :: this
     res = this%mean1_
   end function mean1_ovc
   pure real(real64) function mean2_ovc(this) result(res)
-    class(online_covariance), intent(in) :: this
+    class(online_variance_covariance), intent(in) :: this
     res = this%mean2_
   end function mean2_ovc
   pure real(real64) function var1_ovc(this) result(res)
-    class(online_covariance), intent(in) :: this
+    class(online_variance_covariance), intent(in) :: this
     res = this%m2_1_ / this%cnts_
   end function var1_ovc
   pure real(real64) function sample_var1_ovc(this) result(res)
-    class(online_covariance), intent(in) :: this
+    class(online_variance_covariance), intent(in) :: this
     res = this%m2_1_ / (this%cnts_ - 1)
   end function sample_var1_ovc
   pure real(real64) function var2_ovc(this) result(res)
-    class(online_covariance), intent(in) :: this
+    class(online_variance_covariance), intent(in) :: this
     res = this%m2_2_ / this%cnts_
   end function var2_ovc
   pure real(real64) function sample_var2_ovc(this) result(res)
-    class(online_covariance), intent(in) :: this
+    class(online_variance_covariance), intent(in) :: this
     res = this%m2_2_ / (this%cnts_ - 1)
   end function sample_var2_ovc
   pure real(real64) function covar_ovc(this) result(res)
-    class(online_covariance), intent(in) :: this
+    class(online_variance_covariance), intent(in) :: this
     res = this%c_ / this%cnts_
   end function covar_ovc
   pure real(real64) function sample_covar_ovc(this) result(res)
-    class(online_covariance), intent(in) :: this
+    class(online_variance_covariance), intent(in) :: this
     res = this%c_ / (this%cnts_ - 1)
   end function sample_covar_ovc
 end module online_variance_covariance_m
